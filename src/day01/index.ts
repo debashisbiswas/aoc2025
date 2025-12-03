3f673d1947f070686e033473a955f1309d70f5be
Debashis Biswas <37234687+debashisbiswas@users.noreply.github.com>
2025-12-03 11:33:55 -0700

    day 1 part 1

---
+++ src/day01/index.ts
@@ -0,0 +1,72 @@
+export const rotate = (
+  from: number,
+  direction: "L" | "R",
+  amount: number,
+): number => {
+  if (direction === "L") {
+    let result = from - amount;
+
+    while (result < 0) {
+      result += 100;
+    }
+
+    return result;
+  } else {
+    let result = from + amount;
+
+    while (result > 99) {
+      result -= 100;
+    }
+
+    return result;
+  }
+};
+
+export const parseRotation = (s: string) => {
+  if (s.length === 0) throw Error("empty string");
+
+  const direction = s[0];
+
+  if (direction !== "L" && direction !== "R") {
+    throw Error(`invalid direction: ${direction}`);
+  }
+
+  const amountString = s.substring(1);
+
+  if (amountString.length === 0) {
+    throw Error(`missing amount`);
+  }
+
+  const amount = Number(amountString);
+
+  if (Number.isNaN(amount)) {
+    throw Error(`not a number: ${amountString}`);
+  }
+
+  return {
+    direction,
+    amount,
+  } as const; // preserve narrowed direction type
+};
+
+export const solve = (rotations: ReturnType<typeof parseRotation>[]) => {
+  let dial = 50;
+  let password = 0;
+
+  for (const rotation of rotations) {
+    dial = rotate(dial, rotation.direction, rotation.amount);
+    if (dial === 0) password++;
+  }
+
+  return password;
+};
+
+const file = Bun.file("src/inputs/day01.txt");
+const text = await file.text();
+
+const rotations = text
+  .split("\n")
+  .filter((line) => line.length > 0)
+  .map(parseRotation);
+
+console.log(solve(rotations));
